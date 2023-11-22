214c2cf8b8d1618512d68cbb2720f9639b434026
Devon Layne Gneiting <dtennis@newmexico.cs.byu.edu>
2023-11-22 01:19:45 -0700

    committing files from lab 10

--- 10-lab-proxy-threadpool/proxy.c
+++ 10-lab-proxy-threadpool/proxy.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
 /* Recommended max object size */
 #define MAX_OBJECT_SIZE 102400
@@ -9,21 +14,205 @@ int complete_request_received(char *);
 int parse_request(char *, char *, char *, char *, char *);
 void test_parser();
 void print_bytes(unsigned char *, int);
-
+int open_sfd(int port, int optval);
+int open_server(const char * hostname, const char * port);
+void handle_client(int client_socket);
 
 int main(int argc, char *argv[])
 {
-	test_parser();
+	char * port_str = malloc(16);
+	if (argc > 1) {
+		port_str = argv[1];
+	}
+	int port = atoi(port_str);
+	
 	printf("%s\n", user_agent_hdr);
+	
+	int sfd = open_sfd(port, 1);
+	while(1) {
+		struct sockaddr_in in_addr;
+		socklen_t in_addr_len = sizeof(in_addr);
+		int client_socket = accept(sfd, (struct sockaddr*)&in_addr, &in_addr_len);
+	handle_client(client_socket);
+	}
 	return 0;
 }
 
+int open_sfd(int port, int optval) {
+    // Create a TCP socket
+    int sfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sfd == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // Allow reuse of address and port
+    if (setsockopt(sfd, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof(optval)) == -1) {
+        perror("setsockopt");
+        close(sfd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Bind to the specified port
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port);
+
+    if (bind(sfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        perror("bind");
+        close(sfd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Configure for accepting new clients
+    if (listen(sfd, 10) == -1) {
+        perror("listen");
+        close(sfd);
+        exit(EXIT_FAILURE);
+    }
+
+    return sfd;
+}
+
+int open_server(const char * hostname, const char * port) {
+	struct addrinfo hints, *res;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	if (getaddrinfo(hostname, port, &hints, &res) != 0) {
+		perror("getaddrinfo");
+		return -1;
+	}
+
+	int sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	if (sockfd == -1) {
+		perror("socket");
+		freeaddrinfo(res);
+		return -1;
+	}
+
+	if (connect(sockfd, res->ai_addr, res->ai_addrlen) == -1) {
+		perror("connect");
+		close(sockfd);
+		freeaddrinfo(res);
+		return -1;
+	}
+
+	freeaddrinfo(res);
+	return sockfd;
+}
+
+
+void handle_client(int client_socket) {
+	char buffer[1024]; // Adjust the buffer size as needed
+
+	// Read from the socket into a buffer until the entire HTTP request received
+	ssize_t bytes_received = 0;
+	ssize_t new_bytes = 0;
+	while(1) {
+		new_bytes = recv(client_socket, buffer + bytes_received, sizeof(buffer), 0);
+		bytes_received += new_bytes;
+		if (new_bytes == 0) { break;}
+		new_bytes = 0;
+		if (strstr(buffer, "\r\n\r\n") != NULL || strstr(buffer, "\n\n") != NULL) {
+			//printf("we got to a blank line\n");
+			break;
+		}
+		buffer[bytes_received] = '\0';
+		//printf("buffer: %s\n", buffer);
+		buffer[bytes_received] = '0';
+	}
+	if (bytes_received == 0) { 
+		close(client_socket);
+		return;
+	}
+	buffer[bytes_received] = '\0';
+	//printf("%s\n", buffer);	
+	//print_bytes((unsigned char*)buffer, bytes_received);
+	
+	// Add a null-terminator to the HTTP request
+	buffer[bytes_received] = '\0';
+	// Pass the HTTP request to the parse_request function
+	char method[16], hostname[64], port[8], path[64];
+	parse_request(buffer, method, hostname, port, path);
+	
+	char * host = strcat(hostname, (atoi(port) == 80 ? "" : port));
+	char * user_agent = (char*)user_agent_hdr;
+	size_t total_length = snprintf(NULL, 0, "%s /%s HTTP/1.0\r\nHost: %s\r\n%s\r\nConnection: close\r\nProxy_Connection: close\r\n\r\n", method, path, host, user_agent);
+	
+	char * combined_str = (char*)malloc(total_length);
+	snprintf(combined_str, total_length, "%s /%s HTTP/1.0\r\nHost: %s\r\n%s\r\nConnection: close\r\nProxy-Connection: close\r\n\r\n", method, path, host, user_agent);
+	combined_str[total_length - 1] = '\n';
+	//print_bytes((unsigned char*)combined_str, total_length);
+	//printf("%s\n", combined_str);	
+	int server_socket = open_server(host, port);
+
+	send(server_socket, combined_str, total_length, 0);
+
+	char response_buffer[1024];
+	int server_bytes_received = 0;
+	int new_server_bytes = 0;
+	while((new_server_bytes = recv(server_socket, response_buffer + server_bytes_received, sizeof(response_buffer) - 1, 0)) > 0) {
+		server_bytes_received += new_server_bytes;
+	}
+	response_buffer[server_bytes_received] = '\0';
+	//printf("response: %s\n", response_buffer);
+	
+	send(client_socket, response_buffer, server_bytes_received, 0);
+	// Close the socket
+	close(client_socket);
+
+	if (bytes_received == -1) {
+		perror("recv");
+	}
+}
+
 int complete_request_received(char *request) {
+	if (strstr(request, "\r\n\r\n") != NULL) {
+		return 1;
+	}
 	return 0;
 }
 
 int parse_request(char *request, char *method,
 		char *hostname, char *port, char *path) {
+	if (complete_request_received(request) == 1) {
+		char * beginning = request;
+		char * end = strstr(beginning, " ");
+		strncpy(method, beginning, end - beginning);
+		method[end - beginning] = '\0';
+		beginning = end + 1;
+		end = strstr(beginning, "://");
+		beginning = end + 3;
+		end = strstr(beginning, " ");
+		char * url = malloc(1024);
+		strncpy(url, beginning, end - beginning);
+		char * contains_colon = strstr(url, ":");
+		if (contains_colon != NULL) {
+			end = strstr(beginning, ":");
+			strncpy(hostname, beginning, end - beginning);
+			hostname[end - beginning] = '\0';
+			beginning = end + 1;
+			end = strstr(beginning, "/");
+			strncpy(port, beginning, end - beginning);
+			port[end - beginning] = '\0';
+		}
+		else {
+			end = strstr(beginning, "/");
+			strncpy(hostname, beginning, end - beginning);
+			hostname[end - beginning] = '\0';
+			strcpy(port, "80");
+		}
+		beginning = end + 1;
+		end = strstr(beginning, " ");
+		strncpy(path, beginning, end - beginning);
+		path[end - beginning] = '\0';
+		
+		return 1;
+	}
 	return 0;
 }
 
